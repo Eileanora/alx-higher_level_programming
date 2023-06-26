8150eab5e684152ca6c8495189228b50d2516e41
Yasmeen Hany <96462150+Eileanora@users.noreply.github.com>
2023-06-26 23:15:32 +0300

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,6 +11,6 @@ def safe_print_list_integers(my_list=[], x=0):
     except IndexError:
         pass
     finally:
-        print()
+        if count!=0:
+            print()
         return count
-    
