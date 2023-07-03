e409f45800687e8f58b0f8810db4988388bf56da
yasmeen <yasmeenhany14@gmail.com>
2023-07-03 22:09:58 +0300

    fixed type checking

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter for width"""
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +31,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter for height"""
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
