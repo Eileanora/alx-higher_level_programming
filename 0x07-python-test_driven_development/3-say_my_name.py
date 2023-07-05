f36d3a8bcbe110f5862eb795c81e87570e77c487
yasmeen <yasmeenhany14@gmail.com>
2023-07-05 12:14:14 +0300

    added tests for missing names

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,3 +11,8 @@ def say_my_name(first_name="", last_name=""):
     if type(last_name) is not str or last_name is None:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
