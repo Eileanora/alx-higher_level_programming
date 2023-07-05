0b62bdcd435848a75824d33d66f4b8fa8ccba67d
yasmeen <yasmeenhany14@gmail.com>
2023-07-05 11:57:13 +0300

    fixed error msg and pycodestyle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,26 +2,24 @@
 """Module that divides all elements of a matrix."""
 
 
-def matrix_divided(matrix = [[1]], div = 1):
+def matrix_divided(matrix=[[1]], div=1):
     """Function that divides all elements of a matrix."""
 
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list or matrix is None:
-        raise TypeError("matrix must be a matrix (list of lists) of " \
-                        "integers/floats")
+        raise TypeError(error_msg)
 
     if type(div) is not int and type(div) is not float or div is None:
         raise TypeError("div must be a number")
 
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) " \
-                "of integers/floats")
+            raise TypeError(error_msg)
 
     for row in matrix:
         for element in row:
             if type(element) is not int and type(element) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of " \
-                                "integers/floats")
+                raise TypeError(error_msg)
 
     for row in matrix:
         if len(row) != len(matrix[0]):
@@ -35,6 +33,7 @@ def matrix_divided(matrix = [[1]], div = 1):
         new_matrix.append(list(map(lambda x: round(x / div, 2), row)))
 
     return new_matrix
+
 
 if __name__ == "__main__":
     import doctest
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,7 @@
 My name is <first name> <last name>"""
 
 
-def say_my_name(first_name = "", last_name=""):
+def say_my_name(first_name="", last_name=""):
     """prints My name is <first name> <last name>"""
 
     if type(first_name) is not str or first_name is None:
