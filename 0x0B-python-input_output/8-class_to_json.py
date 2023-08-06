48b8c2cdedfb659e10b91055704baf01a048dc4e
yasmeen <yasmeenhany14@gmail.com>
2023-08-07 01:04:00 +0300

    tasks 8, 9

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+'''Module that returns the dictionary description with simple data structure'''
+
+
+def class_to_json(obj):
+    '''Returns the dictionary description with simple data structure'''
+    return obj.__dict__
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Module that defines a Student class'''
+
+
+class Student:
+    '''class that defines a student'''
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a student'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''Returns the dictionary representation of a Student instance'''
+        return self.__dict__
