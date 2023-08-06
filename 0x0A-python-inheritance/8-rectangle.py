05a1cdca1c3b71fc44ec15f1ad2e2ea1213c90ae
yasmeen <yasmeenhany14@gmail.com>
2023-08-06 15:14:23 +0300

    added class doc

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,6 +4,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    '''Class that represents a rectangle'''
     def __init__(self, width, height):
         '''Constructor'''
         self.integer_validator('width', width)
