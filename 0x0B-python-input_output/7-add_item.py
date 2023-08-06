643b65e36a79569d74fb089c83b130735c6464b3
yasmeen <yasmeenhany14@gmail.com>
2023-08-06 23:57:05 +0300

    fixed doc

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Module that adds all arguments to a Python list"""
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
