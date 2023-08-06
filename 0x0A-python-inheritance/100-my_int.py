e078c89d64754577fec1de04a2d91f6c48da3c06
yasmeen <yasmeenhany14@gmail.com>
2023-08-06 15:30:23 +0300

    adv tasks

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,4 @@
-#!#!/usr/bin/python3
+#!/usr/bin/python3
 '''Module that contains the MyInt class'''
 
 
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,4 +1,4 @@
-#!#!/usr/bin/python3
+#!/usr/bin/python3
 '''Module that  contains the add_attribute function'''
 
 
