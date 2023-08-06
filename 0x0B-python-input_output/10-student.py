0012c17ba599ccf4858e64408326dc6d229a0e6c
yasmeen <yasmeenhany14@gmail.com>
2023-08-07 01:37:36 +0300

    btats

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''Module that defines a Student class'''
+
+
+class Student:
+    '''class that defines a student'''
+    def __init__(self, first_name, last_name, age):
+        '''Initializes a student'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Returns the dictionary representation of a Student instance'''
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''Module that defines pascal triangle'''
+
+
+def pascal_triangle(n):
+    '''Returns a list representing pascal triangle'''
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
