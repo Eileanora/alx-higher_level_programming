d02431062594bed0aeed731a01bfead1d6bd64a2
yasmeen <yasmeenhany14@gmail.com>
2023-09-12 00:37:57 +0300

    fixed semistandard problems

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-let s = "undefined";
-let t = "undefined";
-if (process.argv[2])
-  s = process.argv[2];
-if (process.argv[3])
-  t = process.argv[3];
-console.log(s + " is " + t);
+let s = 'undefined';
+let t = 'undefined';
+if (process.argv[2]) { s = process.argv[2]; }
+if (process.argv[3]) { t = process.argv[3]; }
+console.log(s + ' is ' + t);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-if (Number.isInteger(parseInt(process.argv[2])))
-    console.log("My number: " + parseInt(process.argv[2]));
-else
-    console.log("Not a number");
+if (Number.isInteger(parseInt(process.argv[2]))) {
+  console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
+}
