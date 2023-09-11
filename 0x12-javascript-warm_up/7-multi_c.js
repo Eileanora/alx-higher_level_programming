6dae0daa130cbd8902ea40388bdd4288ad45b0f0
yasmeen <yasmeenhany14@gmail.com>
2023-09-12 00:48:32 +0300

    tasks from 6 to 9

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const x = parseInt(process.argv[2]);
+if (Number.isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (Number.isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
