39a33d1aa953fefcfc98a8be7d43ef1add94cfa7
yasmeen <yasmeenhany14@gmail.com>
2023-09-11 13:06:04 +0300

    fixed task 2, 4, 5 and added tasks from 6 to 16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT score, COUNT(score) AS count
+FROM second_table
+GROUP BY score
+ORDER BY score desc;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- a script that deletes the database hbtn_0c_0 in your MySQL server.
-DROP DATABASE IF EXISTS test_db
+DROP DATABASE IF EXISTS hbtn_0c_0;
