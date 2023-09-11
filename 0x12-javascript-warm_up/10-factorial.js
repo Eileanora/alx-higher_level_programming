e662fa04b207ef7ef5a078ca4c9ec0de8f86fd86
yasmeen <yasmeenhany14@gmail.com>
2023-09-12 00:52:57 +0300

    tasks from 10 to 13

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const num = parseInt(process.argv[2]);
+
+console.log(factorial(num));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const numbers = process.argv.slice(2).map(Number);
+numbers.sort((a, b) => b - a);
+if (numbers.length < 2) {
+  console.log(0);
+} else {
+  console.log(numbers[1]);
+}
