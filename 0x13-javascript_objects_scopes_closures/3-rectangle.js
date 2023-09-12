5570abb4149358c4f147572a1ebfbe625cc4ab1f
yasmeen <yasmeenhany14@gmail.com>
2023-09-13 01:37:04 +0300

    tasks from 0 to 5

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return new Rectangle();
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+## Java script - Objects, Scopes and Closures
