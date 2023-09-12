db87de8d41cfb74ef0239c4a205d6b0b5fa203bb
yasmeen <yasmeenhany14@gmail.com>
2023-09-13 02:07:12 +0300

    fix btats

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const sq = require('5-square.js');
 
-class Square extends Rectangle {
+class Square extends sq {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
