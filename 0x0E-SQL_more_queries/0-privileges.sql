50b85b7e4ca5ee9a57edca2a9fed7324fa67efef
yasmeen <yasmeenhany14@gmail.com>
2023-09-14 11:28:44 +0300

    fixed typos

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Script that lists all privileges of the MySQL users
 -- user_0d+1 and user _0d+2 on server (localhost)
 
-SHOW GRANTS FOR user_0_d1@localhost;
-SHOW GRANTS FOR user_0_d2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
