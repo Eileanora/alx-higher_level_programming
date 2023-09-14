ad6dfa20e10a6a6c9310395d62be504796ba47e5
yasmeen <yasmeenhany14@gmail.com>
2023-09-14 11:22:31 +0300

    fixes

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,7 +2,7 @@
 -- id INT with default value 1 and must be unique
 -- name VARCHAR(256)
 
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@
 -- id INT unique, auto generated, can't be null and is a primary key
 -- name VARCHAR(256) can't be null
 
-CREATE DATABASE IF NOT EXISTS hbtn_od_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
