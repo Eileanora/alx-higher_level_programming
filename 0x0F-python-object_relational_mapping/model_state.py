836bd9395c4c3085e938502c1f4681a0f99d961a
yasmeen <yasmeenhany14@gmail.com>
2023-09-17 16:20:20 +0300

    fix class name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,13 +3,13 @@
  a State and an instance Base = declarative_base()
 '''
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
 
 
-class states(Base):
+class State(Base):
     '''Class definition of a State'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
