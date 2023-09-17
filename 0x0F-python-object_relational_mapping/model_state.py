fe46d3d5124fae028f7fbcb056eb213ba5d50f7e
yasmeen <yasmeenhany14@gmail.com>
2023-09-17 16:18:58 +0300

    change where declarative base is imported from

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
  a State and an instance Base = declarative_base()
 '''
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
