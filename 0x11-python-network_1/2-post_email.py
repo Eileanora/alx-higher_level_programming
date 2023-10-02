05b864bac722248dc570b80211139ef1cc076736
yasmeen <yasmeenhany14@gmail.com>
2023-10-02 12:18:44 +0300

    tasks from 0 to 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''script that takes a URL and an email and sends a \
+POST request to the passed URL with the email as a parameter, \
+and displays the body of the response (decoded in utf-8)'''
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    # convert email to key-value pair
+    # to be able to send it in the body of the post request
+    data = urllib.parse.urlencode({'email': email})
+    # convert data to bytes
+    data = data.encode('ascii')
+
+    with urllib.request.urlopen(url, data) as reponse:
+        print(reponse.read().decode('utf-8'))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''script that takes ina URL, sends a request \
+and displays the body of the response.
+manage exceptions and print the error code'''
+import urllib.request
+import urllib.error
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
