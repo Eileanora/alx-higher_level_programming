c3e0e426a9cee907be09193599052c2eed95a05b
yasmeen <yasmeenhany14@gmail.com>
2023-10-05 12:16:58 +0300

    task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''script that prints the recent 10 commits of a repository by user'''
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    repository = 'rails'
+    owner = 'rails'
+    url = "https://api.github.com/repos/{}/{}/commits?author={}\
+&per_page=10".format(owner, repository, owner)
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
