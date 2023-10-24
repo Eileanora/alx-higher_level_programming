27094b81f0b3821e95a8be06a8b3347e489a5deb
yasmeen <yasmeenhany14@gmail.com>
2023-10-24 12:32:24 +0300

    fix semistanderd

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,12 +2,12 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const file_path = process.argv[3];
+const filePath = process.argv[3];
 request(url, function (err, response, body) {
   if (err) {
     throw err;
   } else {
-    fs.writeFile(file_path, body, 'utf8', function (err2) {
+    fs.writeFile(filePath, body, 'utf8', function (err2) {
       if (err2) {
         throw err2;
       }
